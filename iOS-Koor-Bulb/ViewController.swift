fadda5ff0f091a7d836037497722ff2f785da2e9
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2017-03-03 22:31:27 +0700

    Initial Commit

---
+++ iOS-Koor-Bulb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Koor-Bulb
+//
+//  Created by Nguyen Sy Thanh Son on 3/3/17.
+//  Copyright © 2017 Nguyen Sy Thanh Son. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
